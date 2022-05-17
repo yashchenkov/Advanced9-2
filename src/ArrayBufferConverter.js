a56d84ce83abd5a794752bedd622d95a46d1d4c3
Yashchenko Victor <Andrey@Komp11.local>
2022-05-17 23:23:08 +0300

    final commit

--- src/ArrayBufferConverter.js
+++ src/ArrayBufferConverter.js
@@ -11,16 +11,7 @@ export default class ArrayBufferConverter {
 
   toString() {
   	const buf = new Uint16Array(this.load(getBuffer()));
-  	let result = '';
-    /*result = buf.reduce((elem1) => {
-      String.fromCharCode(elem1);
-    });*/
-    /*buf.reduce((elem1, elem2) => {
-      result += String.fromCharCode(elem1) + String.fromCharCode(elem2);
-    });*/
-  	for(let i = 0; i < buf.length; i++) {
-  	  result += String.fromCharCode(buf[i]);
-  	}
+  	let result = String.fromCharCode(...buf);
   	return result;
   }
 }
